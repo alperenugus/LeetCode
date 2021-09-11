8143b44b394bb5aff1311f895b553bb83662a747
alperenugus <alperenugus@gmail.com>
2021-09-10 18:59:25 -0700

    Talk is cheap, show me the code.

---
+++ Java/ImplementQueueUsingStacks.java
@@ -0,0 +1,73 @@
+/*
+
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+*/
+
+class MyQueue {
+    
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        if(s1.isEmpty()){
+            s1.push(x);
+        }
+        else{
+            
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
+            
+            s1.push(x);
+            
+            while(!s2.isEmpty()){
+                s1.push(s2.pop());
+            }
+            
+        }
+        
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        return s1.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        return s1.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return s1.isEmpty() && s2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
