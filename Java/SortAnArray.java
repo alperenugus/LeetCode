92e093dbbf13115955348ce759e75eb1b817bfcd
alperenugus <alperenugus@gmail.com>
2021-09-14 23:21:23 -0700

    Talk is cheap, show me the code.

---
+++ Java/SortAnArray.java
@@ -0,0 +1,12 @@
+/*
+
+Given an array of integers nums, sort the array in ascending order.
+
+*/
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
